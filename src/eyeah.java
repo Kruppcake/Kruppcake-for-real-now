18694af1a0a6eea2a105ae8f3ac970c14c503835
ETTC <ryan.krupp@mcvts.org>
2019-02-28 16:44:56 -0500

    The most ulusterous program ever made

--- src/eyeah.java
+++ src/eyeah.java
@@ -1,5 +1,12 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class eyeah {
     public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
+        System.out.println("Where the frick my sauce lmao");
+        String yeehaw = kbd.next();
+        System.out.println("The sauce is located at: " + yeehaw);
 
     }
 }
